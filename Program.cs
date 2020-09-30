0005aa7bafc01724bfdfca8f44b6c1856db61510
whuck <70232597+whuck@users.noreply.github.com>
2020-09-30 17:30:43 -0500

    menu loop added

--- Program.cs
+++ Program.cs
@@ -12,18 +12,44 @@ namespace DotNetDbWk6
             var logger = NLog.Web.NLogBuilder.ConfigureNLog(path).GetCurrentClassLogger();
             //instantiate FileParser obj
             FileMaker fm = new FileMaker("data.txt");
-            fm.MakeFile(10);
+            //fm.MakeFile(10);
 
             FileParser fp = new FileParser("data.txt");
-            ArrayList lines = fp.ParseFile();
-            //test run of week creation
-            //string w = "8/30/2020,7|4|10|12|6|10|10";
-            //string w2 = "9/30/2020,3|2|10|12|6|11|11";
-            SleepData sd = new SleepData(lines);
+            //ArrayList lines = fp.ParseFile();
 
-            //sd.AddWeek(w);
-            //sd.AddWeek(w2);
-            Console.WriteLine(sd.ToString());
+            //SleepData sd = new SleepData(lines);
+
+            //Console.WriteLine(sd.ToString());
+
+
+            
+
+            string resp = "";
+            do {
+                Console.WriteLine("Enter 1 to create data file.");
+                Console.WriteLine("Enter 2 to parse data.");
+                Console.WriteLine("Enter anything else to quit.");
+                // input response
+                resp = Console.ReadLine();
+                switch (resp)
+                {
+                    case "1" :
+                    // create data file
+                    // ask a question
+                    Console.WriteLine("How many weeks of data is needed?");
+                    // input the response (convert to int)
+                    int weeks = int.Parse(Console.ReadLine());
+                    fm.MakeFile(weeks);
+                    break;
+                    
+                    case "2" : 
+                    SleepData sd = new SleepData(fp.ParseFile());
+                    Console.WriteLine(sd.ToString());
+                    break;
+
+                    default : break;
+                }
+            } while (resp=="1" || resp=="2");
 
         }
     }
