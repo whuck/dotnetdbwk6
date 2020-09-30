b5f37a161895c1cfdd1c7f4e08017cca2f50fcb1
whuck <70232597+whuck@users.noreply.github.com>
2020-09-30 15:53:04 -0500

    more sleep updates Sdata->Sweek->Sday

--- SleepData.cs
+++ SleepData.cs
@@ -10,34 +10,38 @@ namespace DotNetDbWk6
         {
             this.weeks = new ArrayList();
         }
-        //data.txt line : 8/30/2020,7|4|10|12|6|10|10
+        
         public void AddWeek(string weekData)
         {
+            //weekData  : 8/30/2020,7|4|10|12|6|10|10
             string[] lineItems = weekData.Split(',');
             //lineItems[0] is 8/30/2020
             //lineItems[1] is 7|4|10|12|6|10|10
+            
             //cast date string to DateTime
-            DateTime lineDate = DateTime.Parse(lineItems[0]);
-                    
+            DateTime startDate = DateTime.Parse(lineItems[0]);
+            //cast week hours string in an array
+            string weekHrs = lineItems[1];
+            SleepWeek week = new SleepWeek(startDate, weekHrs);
             //grab hours and toss in an array
-            string[] lineHours = lineItems[1].Split('|');
-                    
-            //iterate through array of string ints and create SleepDay objs for the week
-            int totalHrs = 0;
-            double avgHrs = 0;
+            //string[] lineHours = lineItems[1].Split('|');
+            
+            // //iterate through array of string ints and create SleepDay objs for the week
+            // int totalHrs = 0;
+            // double avgHrs = 0;
 
-            ArrayList week = new ArrayList();
-            for(int i = 0; i < lineHours.Length; i++)
-                {
-                    int dayHours = short.Parse(lineHours[i]);
-                    DateTime dayDate = lineDate.AddDays(i);
-                    SleepDay day = new SleepDay(dayHours,dayDate);
-                    totalHrs+=short.Parse(lineHours[i]);
-                    week.Add(day);
-                }
-            avgHrs = totalHrs / 7;
-            string avg = $"{avgHrs:0.0}";
-            this.weeks.Add(week);
+            // ArrayList week = new ArrayList();
+            // for(int i = 0; i < lineHours.Length; i++)
+            //     {
+            //         int dayHours = short.Parse(lineHours[i]);
+            //         DateTime dayDate = lineDate.AddDays(i);
+            //         SleepDay day = new SleepDay(dayHours,dayDate);
+            //         totalHrs+=short.Parse(lineHours[i]);
+            //         week.Add(day);
+            //     }
+            // avgHrs = totalHrs / 7;
+            // string avg = $"{avgHrs:0.0}";
+            // this.weeks.Add(week);
         }
         
     }
--- SleepWeek.cs
+++ SleepWeek.cs
@@ -8,12 +8,33 @@ namespace DotNetDbWk6
         private ArrayList days {get; set;}
         private DateTime startDate {get; set;}
         private DateTime endDate {get; set;}
-        private Double avgHrs {get; set;}
-        public SleepWeek(ArrayList days, DateTime startDate)
+        private double avgHrs {get; set;}
+        private int totalHrs {get; set;}
+        public SleepWeek(DateTime startDate,string hours)
         {
-            this.days = new ArrayList(days);
+            //this.days = new ArrayList(days);
             this.startDate = startDate;
             this.endDate = startDate.AddDays(7);
+            ArrayList week = new ArrayList();
+            
+            //split string of hours and toss in an array
+            string[] weekHours = hours.Split('|');
+
+            for(int i = 0; i < 6; i++)
+                {
+                    //cast hour to int
+                    int dayHours = short.Parse(weekHours[i]);
+                    //add hours to week's total hours for average calc
+                    this.totalHrs += dayHours;
+                    //make individual day's date by adding loop index to startdate
+                    DateTime dayDate = startDate.AddDays(i);
+                    
+                    SleepDay day = new SleepDay(dayHours,dayDate);
+                    //hours+=short.Parse(hours[i]);
+                    week.Add(day);
+                }  
+            this.avgHrs /= 7;
+            this.days = week;
         }
 
     }
